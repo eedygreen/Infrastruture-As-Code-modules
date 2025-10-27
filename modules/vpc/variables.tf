18f4336798d39ca6e0d8cbccba91737b20de7e1d
Isah Idris <idris.isah2@st.futminna.edu.ng>
2025-10-27 15:07:42 +0100

    modified:   modules/vpc/variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,7 +1,38 @@
-variable "region" {}
-variable "env" {}
-variable "cidr" {}
-variable "public_subnets" {}
-variable "private_subnets" {}
-variable "database_subnets" {}
-variable "elasticache_subnets" {}
+variable "region" {
+  type        = string
+  description = "AWS region"
+}
+
+variable "env" {
+  type        = string
+  description = "Environment name"
+}
+
+variable "cidr" {
+  type        = string
+  description = "VPC CIDR block"
+}
+
+variable "public_subnets" {
+  type        = list(string)
+  default     = []
+  description = "List of public subnet CIDR blocks"
+}
+
+variable "private_subnets" {
+  type        = list(string)
+  default     = []
+  description = "List of private subnet CIDR blocks"
+}
+
+variable "database_subnets" {
+  type        = list(string)
+  default     = []
+  description = "List of database subnet CIDR blocks"
+}
+
+variable "elasticache_subnets" {
+  type        = list(string)
+  default     = []
+  description = "List of elasticache subnet CIDR blocks"
+}
